829eba9917e3f3cb14f05fcf531e3202a56ff569
Mark.Alexieff <Mark.Alexieff@INA-8PMPKX1>
2017-03-11 00:19:20 -0800

    Got the try catch throw stuff working

--- X170-8/Source.cpp
+++ X170-8/Source.cpp
@@ -9,6 +9,7 @@
 // Error conditions to check for:
 // 1. User tries to input non-integers
 // 2. User asks to sum more numbers than there are in the vector.
+// 3. Add if the SUM cannot be represented as an int.  MAX_INT = 2147483647  MIN_INT = -2147483648
 
 // Start standard opening - test change
 
@@ -17,10 +18,15 @@
 #include <vector>
 #include <algorithm>
 #include <cmath>
+#include <limits>
+#include <exception>
 
 using namespace std;
 
-inline void keep_window_open() { char ch; cin >> ch; }
+inline void keep_window_open() { 
+	cin.clear();
+	cin.ignore(numeric_limits<streamsize>::max(), '\n');
+	char ch; cin >> ch; }
 
 inline void simple_error(string s)	// write ``error: s and exit program
 {
@@ -49,7 +55,7 @@ bool yes_no(string s) {
 int get_number_list(vector<int> & nl);
 bool out_number_list(vector<int> & nl);
 int do_the_sum(vector<int> & nl, int c);
-
+class Addition_overflow {};
 
 
 int main() {
@@ -58,28 +64,39 @@ int main() {
 	int val_count{ 0 };
 	bool gp_return{ true };
 
-
-
+	
 	do {
 
 		if (number_list.size() != 0) number_list.clear();
-		
-		cout << "\nPlease enter the numbers of values you want to sum: ";
-		cin >> number_to_sum;
 
-		cout << "\nEnter the values, '|' to end: ";
-		val_count = get_number_list(number_list);
-		gp_return = out_number_list(number_list);
+		try {
+			cin.exceptions(ios_base::badbit | ios_base::failbit);
 
-		if (number_to_sum > number_list.size()) cout << "\nmain: number to sum is too large.\n";
-		else {
-			cout << "\nThe sum of the first " << number_to_sum << " numbers " << "( ";
-			for (int i = 0; i < number_to_sum; ++i) cout << number_list[i] << " ";
-			cout << ") is: " << do_the_sum(number_list, number_to_sum) << "\n";
+			cout << "\nPlease enter the numbers of values you want to sum: ";
+			cin >> number_to_sum;
+
+			cout << "\nEnter the values, '|' to end: ";
+			val_count = get_number_list(number_list);
+			gp_return = out_number_list(number_list);
+
+			if (number_to_sum > number_list.size()) cout << "\nmain: number to sum is too large.\n";
+			else {
+				cout << "\nThe sum of the first " << number_to_sum << " numbers " << "( ";
+				for (int i = 0; i < number_to_sum; ++i) cout << number_list[i] << " ";
+				cout << ") is: " << do_the_sum(number_list, number_to_sum) << "\n";
+			}
 		}
+		catch (exception& e) {
+			cerr << "error: " << e.what() << "\n";
+			simple_error("Caught the exception");
+			return 1;
+		}
+		catch (Addition_overflow) {
+			cerr << "error: Caught Addition_overflow\n";
+			simple_error("Caught another exception");
 
-
-
+		}
+	
 	} while (yes_no("\nWould you like to go again? "));
 }
 
@@ -91,10 +108,12 @@ int get_number_list(vector<int>& nl)
 	cin >> test;
 	while (test != '|') {
 		cin.putback(test);
-		cin >> temp;
-		count += 1;
-		nl.push_back(temp);
-		cin >> test;
+		if (cin >> temp) {
+			count += 1;
+			nl.push_back(temp);
+			cin >> test;
+		}
+		else simple_error("get_number_list: bad input...");
 	}
 	return count;
 }
@@ -117,7 +136,40 @@ bool out_number_list(vector<int>& nl)
 int do_the_sum(vector<int>& nl, int c)    // assumes c <= number of elements in nl - caller must check
 {
 	int the_sum{ 0 };
-	for (int i = 0; i < c;++i) the_sum += nl[i];
+	float test{ 0 };
+	bool okay_to_add{ false };
+
+	for (int i = 0; i < c;++i) {
+		// case 0: sum is 0 and nl[i] is positive
+		// case 02: sum is O and nl[i] is negative  (both are okay cuz number list only has good numbers in it)
+		okay_to_add = false;
+
+		if (the_sum == 0) okay_to_add = true;
+
+		// case 1: sum is positive and nl[i] is positive
+		if (the_sum > 0 && nl[i] >= 0) {
+			cout << INT_MAX << " the_sum = " << the_sum << "nl[i] = " << nl[i] << "\n";
+			cout << "INT_MAX - the sum = " << INT_MAX - the_sum << "\n";
+			if ((INT_MAX - the_sum) >= nl[i])  okay_to_add = true;
+		}
+		
+		// case 2: sum is positive and nl[i] is negative (okay cux any nl[i] is within limits for ints
+		if (the_sum > 0 && nl[i] < 0) okay_to_add = true;
+
+		// case 3: sum is negative and nl[i] is negative
+		if (the_sum < 0 && nl[i] < 0) {
+			if (INT_MIN - the_sum <= nl[i]) okay_to_add = true;
+		}
+
+		// case 4: sum is negative and nl[i] is positive
+		if (the_sum < 0 && nl[i] > 0) okay_to_add = true;
+
+		if (okay_to_add) {
+			the_sum += nl[i];
+			cout << "\n--- Int sum is: " << the_sum << "\n";
+		}
+		else throw(Addition_overflow{});
+	}
 	return the_sum;
 }
 
